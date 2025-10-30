1ec948b4270d7bfdb9453d9e4134b8a2de01299c
ajilkumar <ajilk0325@gmail.com>
2025-10-30 17:25:30 +0530

    Components Added - Added a light ray component in the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 const page = () => {
   return (
-    <div>
-      <h1>Welcome to Next JS 16!</h1>
-    </div>
+    <section>
+      <h1 className="text-center">The Hub for Every Dev <br />Event You Can&apos;t Miss</h1>
+      <p className="text-center mt-5">Hackathons, Meetups and Conferences All in One Place</p>
+    </section>
   )
 }
 
